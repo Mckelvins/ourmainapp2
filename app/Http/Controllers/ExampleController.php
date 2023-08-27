bc20eaf92a924fb1d756a02b33d8c622051291e6
Kalvis Krijnis-Reiniks <kalvis.reiniks@gmail.com>
2023-08-27 07:51:08 +0300

    first commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function homepage(){
+        //imagine we loaded data from database
+        $ourname ='kalvis';
+        $animals = ['cow', 'cat', 'dog'];
+
+        return view('homepage', ['allAnimals'=>$animals,'name'=>$ourname, 'catname' =>'Meavsalot']);
+    }
+
+    public function aboutPage(){
+        return view('single-post');
+    }
+}
